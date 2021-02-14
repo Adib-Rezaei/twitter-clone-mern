a57bfc1b313133c4c740903fd97d9209875433cd
Adib-Rezaei <adibrezaeish@gmail.com>
2021-02-14 20:14:36 +0330

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ if (process.env.NODE_ENV === 'production'){
   app.use(express.static('twitter-clone/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'twitter-clone', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'twitter-clone', 'build', 'index.html'));
   })
 }
 
